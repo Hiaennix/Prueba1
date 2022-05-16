95fc26a8fa9960ed244015a1c70bfdb14f374dd6
liessel3@gmail.com <liessel3@gmail.com>
2022-05-16 10:37:32 -0400

    guardado 1

---
+++ Prueba1Liessel/Prueba1Liessel/Buque.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba1Liessel
+{
+    internal class Buque
+    {
+        private string codigo;
+        private string nombre;
+        private string pais;
+        private int cantContainers;
+        private int cantContainersC;
+        private int gastoTransp;
+        private List<Container> listaContainers;
+
+        public void SubirContainer(NuevoContainer:Container): bool;
+    }
+}
---
+++ Prueba1Liessel/Prueba1Liessel/Container.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba1Liessel
+{
+    internal class Container
+    {
+        private string codigo;
+        private string marca;
+        private int CapMax;
+        private byte tamaño;
+        private bool esRefrigerado;
+        private int pesoActual;
+
+        public Buque buque;
+
+        public void SacarPeso();
+
+        public int ValorPagoInspeccion();
+        public int CalcularGastosEnvio();
+    }
+}
